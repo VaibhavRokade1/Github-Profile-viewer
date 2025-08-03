2401a3e78186c22b17eddf6a6d20c1c9b3f92e1b
Vaibhav Rokade <rokadevaibhav04@.com>
2025-08-02 16:11:05 -0930

    ...

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { useFormStatus } from "react-dom";
 import useGithubUser from "./useGithubUser";
@@ -42,48 +41,3 @@ function SearchBar() {
 }
 
 export default SearchBar;
-=======
-import React, { useState } from "react";
-import { useFormStatus } from "react-dom";
-import useGithubUser from "./useGithubUser";
-import Github from "./Github";
-
-function SearchBar() {
-  const { username, setUsername, pending, data, handleData } = useGithubUser();
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (username) handleData();
-  };
-
-  return (
-    <>
-      <form
-        onSubmit={onSubmit}
-        className="flex flex-col sm:flex-row gap-4 mb-6 w-full"
-      >
-        <input
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-          value={username}
-          type="text"
-          placeholder="Enter GitHub username"
-          className="px-5 py-3 w-full rounded-xl bg-white border border-gray-300 shadow-inner focus:outline-none focus:ring-2 focus:ring-teal-500 text-gray-800 transition duration-300"
-        />
-        <button
-          disabled={pending}
-          className="bg-gradient-to-r from-teal-500 to-teal-700 text-white px-6 py-3 rounded-xl hover:shadow-xl hover:scale-105 transition-all duration-300 font-semibold hover:cursor-pointer"
-        >
-          {pending ? "Searching..." : "Search"}
-        </button>
-      </form>
-
-      {/* GitHub User Card */}
-      <Github {...data} />
-    </>
-  );
-}
-
-export default SearchBar;
->>>>>>> 7cac94f50ba4b3b6ebfb9f76f1ecc2d0bd20b879
--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 
 function useGithubUser() {
@@ -29,35 +28,3 @@ function useGithubUser() {
 }
 
 export default useGithubUser;
-=======
-import React, { useState } from "react";
-
-function useGithubUser() {
-  const [username, setUsername] = useState("");
-  const [data, setData] = useState(null);
-  const [pending, setPending] = useState(false);
-
-  const handleData = () => {
-    try {
-      setPending(true);
-      setTimeout(async () => {
-        const res = await fetch(`https://api.github.com/users/${username}`);
-        if (!res.ok)
-          throw new Error("Responce Error :dataomething went wrong...!");
-
-        const data = await res.json();
-
-        setData(data ?? null);
-
-        setPending(false);
-      }, 2000);
-    } catch (err) {
-      console.log("Invalid API :" + err);
-    }
-  };
-
-  return { username, setUsername, pending, data, handleData };
-}
-
-export default useGithubUser;
->>>>>>> 7cac94f50ba4b3b6ebfb9f76f1ecc2d0bd20b879
